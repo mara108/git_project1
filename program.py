2d4264c8d950537ced906893dfea134231f8436b
mara108 <mara_bel@mail.ru>
2023-11-21 21:01:03 +0700

    New day — new print

--- program.py
+++ program.py
@@ -5,7 +5,6 @@ def my_superfunction():
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Noch einmal')
     print('Again')
     print('UFO came and added this line')
     print("New feature")
